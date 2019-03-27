ca79f74d6719f324ebd8d7a4195d2de41ee4eae7
布吉岛 <little3201@163.com>
2019-03-27 20:35:51 +0800

    修改注册中心和配置中心为consul

--- src/main/java/top/abeille/gateway/AbeilleGatewayApplication.java
+++ src/main/java/top/abeille/gateway/AbeilleGatewayApplication.java
@@ -2,8 +2,10 @@ package top.abeille.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AbeilleGatewayApplication {
 
     public static void main(String[] args) {
