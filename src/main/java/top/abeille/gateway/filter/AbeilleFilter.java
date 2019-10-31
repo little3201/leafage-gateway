8177cfbb3dcca96f661c07377f83b3accdb9bd0b
李文强 <liwenqiang@tseverday.com>
2019-10-31 14:02:09 +0800

    修改配置

---
+++ src/main/java/top/abeille/gateway/filter/AbeilleFilter.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019. Abeille All Right Reserved.
+ */
+package top.abeille.gateway.filter;
+
+/**
+ * 网关过滤配置
+ *
+ * @author liwenqiang 2019-10-29
+ */
+public class AbeilleFilter {
+
+
+}
