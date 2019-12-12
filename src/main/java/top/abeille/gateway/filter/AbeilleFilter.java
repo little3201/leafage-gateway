c2304d9bffc1d9e34fd64da874392abbe7b24d85
李文强 <liwenqiang@tseverday.com>
2019-12-12 12:40:39 +0800

    添加过滤器

--- src/main/java/top/abeille/gateway/filter/AbeilleFilter.java
+++ src/main/java/top/abeille/gateway/filter/AbeilleFilter.java
@@ -3,22 +3,29 @@
  */
 package top.abeille.gateway.filter;
 
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
 /**
  * 网关过滤配置
  *
  * @author liwenqiang 2019-10-29
  */
-public class AbeilleFilter {
+public class AbeilleFilter implements GlobalFilter {
 
     private static final String ACCESS_TOKEN = "access_token";
 
-//    @Override
-//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-//        String param = exchange.getRequest().getQueryParams().getFirst(ACCESS_TOKEN);
-//        if (StringUtils.isBlank(param)) {
-//            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-//            return exchange.getResponse().setComplete();
-//        }
-//        return chain.filter(exchange);
-//    }
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String param = exchange.getRequest().getQueryParams().getFirst(ACCESS_TOKEN);
+        if (StringUtils.isBlank(param)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
 }
