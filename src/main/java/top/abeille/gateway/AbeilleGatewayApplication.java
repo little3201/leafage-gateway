5aabc30dca020eaad50320a7fe0be04b2201327b
李文强 <liwenqiang@tseverday.com>
2019-12-13 09:44:20 +0800

    代码格式化

--- src/main/java/top/abeille/gateway/AbeilleGatewayApplication.java
+++ src/main/java/top/abeille/gateway/AbeilleGatewayApplication.java
@@ -5,7 +5,6 @@ package top.abeille.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author liwenqiang
