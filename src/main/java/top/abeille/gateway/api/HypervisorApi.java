62dae07b027cc89a8fa2f38429b3884099722cfe
李文强 <liwenqiang@tseverday.com>
2020-06-10 19:33:15 +0800

    认证、鉴权放在网关进行

---
+++ src/main/java/top/abeille/gateway/api/HypervisorApi.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2019. Abeille All Right Reserved.
+ */
+package top.abeille.gateway.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import top.abeille.gateway.api.bo.UserBO;
+
+/**
+ * 用户api
+ *
+ * @author liwenqiang 2019-03-03 22:55
+ **/
+@FeignClient(name = "abeille-basic-hypervisor", fallback = HypervisorApiFallBack.class)
+public interface HypervisorApi {
+
+    /**
+     * 根据传入的手机号: mobile 查询信息
+     *
+     * @param mobile 手机号
+     * @return 如果查询到数据，返回查询到的信息，否则返回404状态码
+     */
+    @GetMapping("/user/mobile/{mobile}")
+    Mono<UserBO> fetchUserByMobile(@RequestParam String mobile);
+
+    /**
+     * 根据传入的邮箱: email 查询信息
+     *
+     * @param email 邮箱
+     * @return 如果查询到数据，返回查询到的信息，否则返回404状态码
+     */
+    @GetMapping("/user/email/{email}")
+    Mono<UserBO> fetchUserByEmail(@RequestParam String email);
+
+    /**
+     * 根据用户业务ID查询角色信息
+     *
+     * @param businessId 用户业务ID
+     * @return 角色业务ID集合
+     */
+    @GetMapping("/role/relation/{businessId}")
+    Flux<String> retrieveRoles(@RequestParam String businessId);
+}
---
+++ src/main/java/top/abeille/gateway/config/AbeilleSecurityConfig.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2019. Abeille All Right Reserved.
+ */
+package top.abeille.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.rsa.crypto.KeyStoreKeyFactory;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.ServerAuthenticationFailureHandler;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
+import top.abeille.gateway.api.HypervisorApi;
+import top.abeille.gateway.handler.AbeilleFailureHandler;
+import top.abeille.gateway.handler.AbeilleSuccessHandler;
+import top.abeille.gateway.security.UserDetailsServiceImpl;
+
+import java.security.KeyPair;
+import java.security.interfaces.RSAPublicKey;
+
+/**
+ * spring security 配置
+ *
+ * @author liwenqiang 2019/7/12 17:51
+ */
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class AbeilleSecurityConfig {
+
+    private static final String KEY_STORE = "jwt/abeille-top-jwt.jks";
+    private static final String KEY_PASS = "abeille-top";
+    private static final String ALIAS = "abeille-top-jwt";
+
+    private final HypervisorApi hypervisorApi;
+
+    public AbeilleSecurityConfig(HypervisorApi hypervisorApi) {
+        this.hypervisorApi = hypervisorApi;
+    }
+
+    /**
+     * 密码配置，使用BCryptPasswordEncoder
+     */
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ReactiveUserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl(hypervisorApi);
+    }
+
+    /**
+     * 安全配置
+     */
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.formLogin().authenticationSuccessHandler(authenticationSuccessHandler())
+                .authenticationFailureHandler(authenticationFailureHandler())
+                .and().csrf().disable()
+                .authorizeExchange().pathMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyExchange().authenticated()
+                .and().exceptionHandling()
+                .and().oauth2ResourceServer().jwt().jwtDecoder(jwtDecoder());
+        return http.build();
+    }
+
+    /**
+     * 登陆成功后执行的处理器
+     */
+    private ServerAuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new AbeilleSuccessHandler();
+    }
+
+    /**
+     * 登陆失败后执行的处理器
+     */
+    private ServerAuthenticationFailureHandler authenticationFailureHandler() {
+        return new AbeilleFailureHandler();
+    }
+
+    /**
+     * JWT 解码器
+     */
+    private ReactiveJwtDecoder jwtDecoder() {
+        return new NimbusReactiveJwtDecoder(readRSAPublicKey());
+    }
+
+    /**
+     * 获取RSAPublicKey
+     */
+    private RSAPublicKey readRSAPublicKey() {
+        ClassPathResource ksFile = new ClassPathResource(KEY_STORE);
+        KeyStoreKeyFactory ksFactory = new KeyStoreKeyFactory(ksFile, KEY_PASS.toCharArray());
+        KeyPair keyPair = ksFactory.getKeyPair(ALIAS);
+        return (RSAPublicKey) keyPair.getPublic();
+    }
+}
