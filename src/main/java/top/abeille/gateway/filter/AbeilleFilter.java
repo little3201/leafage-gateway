fc87bbb9de09f74a95c885526de02d0156a7f81b
little3201 <little3201@163.com>
2020-09-09 23:54:24 +0800

    删除hypervisor的handler; 添加gateway的filter

--- src/main/java/top/abeille/gateway/filter/AbeilleFilter.java
+++ src/main/java/top/abeille/gateway/filter/AbeilleFilter.java
@@ -7,6 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.reactive.result.view.CsrfRequestDataValueProcessor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -15,7 +19,8 @@ import reactor.core.publisher.Mono;
  *
  * @author liwenqiang 2019-10-29
  */
-public class AbeilleFilter implements GlobalFilter {
+@Component
+public class AbeilleFilter implements GlobalFilter, Ordered {
 
     private final Logger log = LoggerFactory.getLogger(AbeilleFilter.class);
 
@@ -23,6 +28,16 @@ public class AbeilleFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String path = exchange.getRequest().getURI().getPath();
         log.info("请求路径：{}", path);
+        if (path.equals("/login")) {
+            Mono<CsrfToken> csrfToken = exchange.getAttribute(CsrfToken.class.getName());
+            exchange.getAttributes()
+                    .put(CsrfRequestDataValueProcessor.DEFAULT_CSRF_ATTR_NAME, csrfToken);
+        }
         return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
     }
 }
