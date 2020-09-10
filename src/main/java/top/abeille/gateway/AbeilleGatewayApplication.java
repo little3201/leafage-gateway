f27d33425be9cd68dbe1be13f0dd8be406456b91
李文强 <liwenqiang@tseverday.com>
2020-09-10 10:30:23 +0800

    添加负载均衡配置

--- src/main/java/top/abeille/gateway/AbeilleGatewayApplication.java
+++ src/main/java/top/abeille/gateway/AbeilleGatewayApplication.java
@@ -5,12 +5,21 @@ package top.abeille.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * @author liwenqiang
  */
 @SpringBootApplication
 public class AbeilleGatewayApplication {
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AbeilleGatewayApplication.class, args);
