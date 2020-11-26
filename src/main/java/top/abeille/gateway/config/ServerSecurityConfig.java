5b5b3d71bab94a05f59ffac926ce234fa015290c
little3201 <little3201@163.com>
2020-11-26 16:18:24 +0800

    精简依赖

--- src/main/java/top/abeille/gateway/config/ServerSecurityConfig.java
+++ src/main/java/top/abeille/gateway/config/ServerSecurityConfig.java
@@ -5,17 +5,14 @@ package top.abeille.gateway.config;
 
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.rsa.crypto.KeyStoreKeyFactory;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.ServerAuthenticationFailureHandler;
@@ -27,9 +24,6 @@ import top.abeille.gateway.handler.AbeilleFailureHandler;
 import top.abeille.gateway.handler.AbeilleSuccessHandler;
 import top.abeille.gateway.service.AbeilleUserDetailsService;
 
-import java.security.KeyPair;
-import java.security.interfaces.RSAPublicKey;
-
 /**
  * spring security 配置
  *
@@ -37,10 +31,6 @@ import java.security.interfaces.RSAPublicKey;
  */
 @EnableWebFluxSecurity
 public class ServerSecurityConfig {
-
-    private static final String KEY_STORE = "jwt/abeille-top-jwt.jks";
-    private static final String KEY_PASS = "abeille-top";
-    private static final String ALIAS = "abeille-top-jwt";
 
     private final HypervisorApi hypervisorApi;
 
@@ -69,7 +59,7 @@ public class ServerSecurityConfig {
      */
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.oauth2ResourceServer(o -> o.jwt().jwtDecoder(jwtDecoder()))
+        http.oauth2ResourceServer(Customizer.withDefaults())
                 .formLogin(f -> f.authenticationSuccessHandler(authenticationSuccessHandler())
                         .authenticationFailureHandler(authenticationFailureHandler()))
                 .logout(l -> l.logoutSuccessHandler(new HttpStatusReturningServerLogoutSuccessHandler()))
@@ -95,20 +85,4 @@ public class ServerSecurityConfig {
         return new AbeilleFailureHandler();
     }
 
-    /**
-     * JWT 解码器
-     */
-    private ReactiveJwtDecoder jwtDecoder() {
-        return new NimbusReactiveJwtDecoder(readRSAPublicKey());
-    }
-
-    /**
-     * 获取RSAPublicKey
-     */
-    private RSAPublicKey readRSAPublicKey() {
-        ClassPathResource ksFile = new ClassPathResource(KEY_STORE);
-        KeyStoreKeyFactory ksFactory = new KeyStoreKeyFactory(ksFile, KEY_PASS.toCharArray());
-        KeyPair keyPair = ksFactory.getKeyPair(ALIAS);
-        return (RSAPublicKey) keyPair.getPublic();
-    }
 }
