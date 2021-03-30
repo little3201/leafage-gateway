1e66fbead9486dc926c877e51b00820a02622393
little3201 <little3201@163.com>
2021-03-30 19:33:50 +0800

    添加邮件发送的方法，及测试类

---
+++ src/main/java/io/leafage/gateway/service/EmailService.java
@@ -0,0 +1,36 @@
+package io.leafage.gateway.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+
+    private static final Logger log = LoggerFactory.getLogger(EmailService.class);
+
+    @Resource
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(String target, String subject, String text) {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom("little3201@163.com");
+            helper.setTo(target);
+            helper.setSubject("[Leafage] " + subject);
+            helper.setText(text);
+            this.javaMailSender.send(message);
+        } catch (MailException | MessagingException ex) {
+            log.error("Send email to {} error: ", target, ex);
+        }
+    }
+
+}
