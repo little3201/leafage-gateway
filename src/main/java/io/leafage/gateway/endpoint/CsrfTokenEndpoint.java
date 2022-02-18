bb73e193ea4b3f51b8da895f30ac5303e37a6a48
little3201 <little3201@163.com>
2022-02-18 15:25:09 +0800

    更新security

--- src/main/java/io/leafage/gateway/endpoint/CsrfTokenEndpoint.java
+++ src/main/java/io/leafage/gateway/endpoint/CsrfTokenEndpoint.java
@@ -1,6 +1,5 @@
 package io.leafage.gateway.endpoint;
 
-import org.springframework.http.ResponseCookie;
 import org.springframework.security.web.server.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +19,7 @@ public class CsrfTokenEndpoint {
     public Mono<Void> csrfToken(ServerWebExchange exchange) {
         Mono<CsrfToken> csrfToken = exchange.getAttribute(CsrfToken.class.getName());
         if (Objects.nonNull(csrfToken)) {
-            return csrfToken.doOnSuccess(token -> exchange.getResponse()
-                    .addCookie(ResponseCookie.from(token.getParameterName(), token.getToken())
-                            .httpOnly(true).build())).then();
+            return csrfToken.doOnSuccess(csrf -> exchange.getResponse()).then();
         }
         return Mono.empty();
     }
